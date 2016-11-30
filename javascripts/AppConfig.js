f45ca83c3bc12ce752259f9a29a5547b8d17ec8c
jessica kilby <jessicakilby@gmail.com>
2016-11-29 18:00:44 -0600

    AppConfig.js

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -2,9 +2,25 @@
 
 app.config(function($routeProvider){
 	$routeProvider
-		.when('/', {
+		.when('/auth', {
+			templateUrl:'partials/auth.html',
+			controller: 'authCtrl'
+		})
+		.when('/pins/', {
 			templateUrl:'partials/searchImgur.html',
 			controller: 'searchCtrl'
+		})
+		.when('', {
+			templateUrl:'partials/.html',
+			controller: 'Ctrl'
+		})
+		.when('', {
+			templateUrl:'partials/.html',
+			controller: 'Ctrl'
+		})
+		.when('/logout', {
+			templateUrl:'partials/auth.html',
+			controller: 'authCtrl'
 		})
 		.otherwise('/');
 });
